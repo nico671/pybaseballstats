aafe2e53e6a8b8fe72d615e22fc54ce0f9035eaa
Nico Carbone <carbone.nicolas0@gmail.com>
2025-01-24 09:18:37 -0500

    good start on fangraphs batting

--- pybaseballstats/fangraphs.py
+++ pybaseballstats/fangraphs.py
@@ -5,14 +5,10 @@ import pandas as pd
 import polars as pl
 import requests
 from bs4 import BeautifulSoup
+from polars import selectors as cs
+from tqdm import tqdm
 
-url = "https://www.fangraphs.com/leaders/major-league?pos={pos}&stats=bat&lg={league}&qual={qual}&type={stat_type}&season={end_season}&season1={start_season}&ind=0&startdate={start_date}&enddate={end_date}&month=0&team=0&pagenum=1&pageitems=2000000000"
-
-# pos options: c, 1b,2b,3b, ss, lf, cf, rf, dh, of, p, all
-# qual options: y, n
-# league options: "", "al", "nl"
-# start date, end date are strings in the format "yyyy-mm-dd"
-# stat options: 8 (dashboard), 0 (standard), 1 (advanced), 2 (batted ball), 3 (win_probability), 6 (value), 23 (+stats),24 (statcast), 48 (violations)
+url = "https://www.fangraphs.com/leaders/major-league?pos={pos}&stats=bat&lg={league}&qual={min_at_bats}&type={stat_type}&season={end_season}&season1={start_season}&ind=0&startdate={start_date}&enddate={end_date}&month=0&team=0&pagenum=1&pageitems=2000000000"
 
 
 # Define the available stat types as an Enum
@@ -26,24 +22,62 @@ class FangraphsBattingStatType(Enum):
     PLUS_STATS = 23
     STATCAST = 24
     VIOLATIONS = 48
+    SPORTS_INFO_PITCH_TYPE = 4
+    SPORTS_INFO_PITCH_VALUE = 7
+    SPORTS_INFO_PLATE_DISCIPLINE = 5
+    STATCAST_PITCH_TYPE = 9
+    STATCAST_VELO = 10
+    STATCAST_H_MOVEMENT = 11
+    STATCAST_V_MOVEMENT = 12
+    STATCAST_PITCH_TYPE_VALUE = 13
+    STATCAST_PITCH_TYPE_VALUE_PER_100 = 14
+    STATCAST_PLATE_DISCIPLINE = 15
+
+
+class FangraphsBattingPosTypes(Enum):
+    CATCHER = "c"
+    FIRST_BASE = "1b"
+    SECOND_BASE = "2b"
+    THIRD_BASE = "3b"
+    SHORTSTOP = "ss"
+    LEFT_FIELD = "lf"
+    CENTER_FIELD = "cf"
+    RIGHT_FIELD = "rf"
+    DESIGNATED_HITTER = "dh"
+    OUTFIELD = "of"
+    PITCHER = "p"
+    NON_PITCHER = "np"
+    ALL = "all"
+
+    def __str__(self):
+        return self.value
+
+
+class FangraphsLeagueTypes(Enum):
+    ALL = ""
+    NATIONAL_LEAGUE = "nl"
+    AMERICAN_LEAGUE = "al"
+
+    def __str__(self):
+        return self.value
 
 
 def get_table_data(
-    stat_type, pos, league, start_date, end_date, qual, start_season, end_season
+    stat_type, pos, league, start_date, end_date, min_at_bats, start_season, end_season
 ):
+    url = "https://www.fangraphs.com/leaders/major-league?pos={pos}&stats=bat&lg={league}&qual={min_at_bats}&type={stat_type}&season={end_season}&season1={start_season}&ind=0&startdate={start_date}&enddate={end_date}&month=0&team=0&pagenum=1&pageitems=2000000000"
+    url = url.format(
+        pos=pos,
+        league=league,
+        min_at_bats=min_at_bats,
+        stat_type=stat_type,
+        start_date=start_date,
+        end_date=end_date,
+        start_season=start_season,
+        end_season=end_season,
+    )
     # Assuming `cont` contains the HTML content
-    cont = requests.get(
-        url.format(
-            pos="all",
-            league="",
-            stat_type=stat_type,
-            start_season=1900,
-            end_season=2024,
-            qual="y",
-            start_date="",
-            end_date="",
-        )
-    ).content.decode("utf-8")
+    cont = requests.get(url).content.decode("utf-8")
 
     # Parse the HTML content with BeautifulSoup
     soup = BeautifulSoup(cont, "html.parser")
@@ -76,6 +110,14 @@ def get_table_data(
             col_id = cell.get("data-col-id")
 
             if col_id and col_id != "divider":
+                # if col_id == "Name":
+                #     row_data[col_id] = cell.find("a").text
+                #     if cell.find("a"):
+                #         row_data[col_id] = cell.find("a").text
+                #     elif cell.find("span"):
+                #         row_data[col_id] = cell.find("span").text
+                #     else:
+                #         text = cell.text.strip()
                 if cell.find("a"):
                     row_data[col_id] = cell.find("a").text
                 elif cell.find("span"):
@@ -89,11 +131,15 @@ def get_table_data(
                             row_data[col_id] = float(text) if "." in text else int(text)
                         except ValueError:
                             row_data[col_id] = text
+                        except Exception as e:
+                            print(e)
+                            print(cell.attrs["data-col-id"])
+                            row_data[col_id] = text
         # Print row_data for debugging
         data.append(row_data)
 
     # Create a Polars DataFrame from the extracted data
-    df = pl.DataFrame(data)
+    df = pl.DataFrame(data, infer_schema_length=None)
     return df
 
 
@@ -106,95 +152,84 @@ def show_batting_pos_options():
     print("c,1b,2b,3b,ss,lf,cf,rf,dh,of,p,all")
 
 
-def fangraphs_batting_date_range(
-    start_date: str,
-    end_date: str,
+# TODO: Add more options
+# - Add support for specifying team (team=) options are given by ints so need to make an enum for that
+# - add support for restricting only to active roster players (rost=) (0 for all, 1 for active roster)
+# - add support for season type (postseason=) ("" for regular season, "Y" for all postseason, "W" for world series, "L" for league championship series, "D" for division series, "F" for wild card game)
+# - add support for handedness (hand=) ("" for all, "R" for right handed batters, "L" for left handed batters, "S" for switch hitters)
+# - add support for age (age=) ("start_age,end_age")
+def fangraphs_batting_range(
+    start_date: str = None,
+    end_date: str = None,
+    start_season: str = None,
+    end_season: str = None,
     stat_types: List[FangraphsBattingStatType] = None,
     return_pandas: bool = False,
-    pos: str = "all",
-    league: str = "",
-    qual: str = "y",
+    pos: FangraphsBattingPosTypes = FangraphsBattingPosTypes.ALL,
+    league: FangraphsLeagueTypes = FangraphsLeagueTypes.ALL,
+    min_at_bats: str = "y",
 ) -> pl.DataFrame | pd.DataFrame:
-    """Pulls Fangraphs batting data for a date range.
+    """Pulls batting data from Fangraphs for a given date range or season range. Additional options include filtering by position and league, as well as the ability to specify which stats to pull.
 
     Args:
-        start_date (str): format "yyyy-mm-dd", ex) "2021-04-01"
-        end_date (str): format "yyyy-mm-dd", ex) "2021-04-01"
-        stat_types (List[FangraphsBattingStatType], optional): List of what Fangraphs stat types to include, more information can be found by calling pyb.show_fangraphs_stat_types(). Defaults to None, meaning all stat types will be returned.
-        return_pandas (bool, optional): whether to return a Polars Dataframe (False) or a Pandas Dataframe (True). Defaults to False.
-        pos (str, optional): What positions to return data for. More information can be found by calling pyb.show_batting_pos_options(). Defaults to "all".
-        league (str, optional): What league to return data for, options are ""(all), "nl", "al". Defaults to "".
-        qual (str, optional): whether or not to restrict to qualified batters, to return unqualified batters pass "n" as the argument. Defaults to "y".
+        start_date (str, optional): First date for which you want to pull data for, format should follow "yyyy-mm-dd" (ex. ("2024-04-01")). Defaults to None.
+        end_date (str, optional): Last date for which you want to pull data for, format should follow "yyyy-mm-dd" (ex. ("2024-06-01")). Defaults to None.
+        start_season (str, optional): First season for which you want to pull data for, format should follow "yyyy" (ex. ("2023")). Defaults to None.
+        end_season (str, optional): Last season for which you want to pull data for, format should follow "yyyy" (ex. ("2024")). Defaults to None.
+        stat_types (List[FangraphsBattingStatType], optional): What stat types to include in the data. Defaults to None (all data types will be retrieved).
+        return_pandas (bool, optional): Should the returned dataframe be a Polars Dataframe (False) or a Pandas dataframe (True). Defaults to False.
+        pos (FangraphsBattingPosTypes, optional): What batter positions you want to include in your search. Defaults to FangraphsBattingPosTypes.ALL.
+        league (FangraphsLeagueTypes, optional): What leagues you want included in your search. Defaults to FangraphsLeagueTypes.ALL.
+        min_at_bats (str, optional): Minimum number of at bats to be included in the dataset (ex min_at_bats="123"). Defaults to "y" (qualified hitters).
+
+    Raises:
+        ValueError: If both date range (start_date and end_date) and season range (start_season and end_season) are not provided.
+        ValueError: If stat_types is an empty list (if you are trying to get all stat_types pass in None).
 
     Returns:
-        pl.DataFrame | pd.DataFrame: The requested data as a Polars or Pandas DataFrame.
+        pl.DataFrame | pd.DataFrame: A Polars or Pandas DataFrame containing the requested data.
     """
+    if (start_date is None or end_date is None) and (
+        start_season is None or end_season is None
+    ):
+        raise ValueError(
+            "Either start_date and end_date must not be None or start_season and end_season must not be None"
+        )
     df_list = []
     if stat_types is None:
-        stat_types = FangraphsBattingStatType
-    if len(stat_types) == 0:
+        stat_types = {}
+        for stat_type in FangraphsBattingStatType:
+            stat_types[stat_type] = stat_type.value
+    elif len(stat_types) == 0:
+        raise ValueError("stat_types must not be an empty list")
+    if min_at_bats != "y":
         print(
-            "Warning: No stat types provided, returning None, to return all stattypes, pass in None."
+            "Warning: setting a custom minimum at bats value may result in missing data"
         )
-        return None
-    for stat_type in stat_types:
-        print(f"Fetching data for {stat_type}...")
+    for stat_type in tqdm(stat_types, desc="Fetching data"):
+        # print(f"Fetching data for {stat_type}...")
+        print(league)
         df = get_table_data(
-            stat_type=stat_types[stat_type.value],
+            stat_type=stat_types[stat_type],
             pos=pos,
             league=league,
-            start_date=start_date,
-            end_date=end_date,
-            qual=qual,
-            start_season="",
-            end_season="",
+            start_date=start_date if start_date is not None else "",
+            end_date=end_date if end_date is not None else "",
+            min_at_bats=min_at_bats,
+            start_season=start_season if start_season is not None else "",
+            end_season=end_season if end_season is not None else "",
         )
         if df is not None:
-            print(f"Data fetched for {stat_type}")
+            # print(f"Data fetched for {stat_type}")
             df_list.append(df)
         else:
             print(f"Warning: No data returned for {stat_type}")
-    df = pl.concat(df_list, how="diagonal")
-    df = df.select(pl.col("Name").drop_nulls())
-    return df.to_pandas() if return_pandas else df
 
-
-def fangraphs_batting_season_range(
-    start_season,
-    end_season,
-    stat_types,
-    return_pandas=False,
-    pos="all",
-    league="",
-    qual="y",
-) -> pl.DataFrame | pd.DataFrame:
-    df_list = []
-    if stat_types is None:
-        stat_types = FangraphsBattingStatType
-    if len(stat_types) == 0:
-        print(
-            "Warning: No stat types provided, returning None, to return all stattypes, pass in None."
+    df = df_list[0]
+    for i in range(1, len(df_list)):
+        df = df.join(df_list[i], on="Name", how="full").select(
+            ~cs.ends_with("_right"),
         )
-        return None
-    for stat_type in stat_types:
-        print(f"Fetching data for {stat_type}...")
-        df = get_table_data(
-            stat_type=stat_types[stat_type.value],
-            pos=pos,
-            league=league,
-            start_date="",
-            end_date="",
-            qual=qual,
-            start_season=start_season,
-            end_season=end_season,
-        )
-        if df is not None:
-            print(f"Data fetched for {stat_type}")
-            df_list.append(df)
-        else:
-            print(f"Warning: No data returned for {stat_type}")
-    df = pl.concat(df_list, how="diagonal")
-    df = df.select(pl.col("Name").drop_nulls())
     return df.to_pandas() if return_pandas else df
 
 
