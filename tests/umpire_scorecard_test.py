2623617eaf71e324efc1f751e94ab0f3e0470e93
Nico Carbone <carbone.nicolas0@gmail.com>
2025-02-16 19:21:16 -0500

    Bump package version to 0.1.1, update usage documentation for Fangraphs, and add umpire scorecard usage documentation

--- tests/umpire_scorecard_test.py
+++ tests/umpire_scorecard_test.py
@@ -256,7 +256,7 @@ def test_team_umpire_stats_date_range_bad_inputs():
             return_pandas=False,
         )
     with pytest.raises(ValueError):
-        data = team_umpire_stats_date_range(
+        data = team_umpire_stats_date_range(  # noqa: F841
             start_date="2024-04-01",
             end_date="2024-05-01",
             season_type="postseason_only",
