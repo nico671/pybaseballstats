5be978afc006c1e09bd5da4fae8be047e638b2e7
Nico Carbone <carbone.nicolas0@gmail.com>
2025-02-28 10:22:16 -0500

    Add franchise_draft_order function with data validation and parsing logic

--- src/pybaseballstats/draft.py
+++ src/pybaseballstats/draft.py
@@ -28,7 +28,6 @@ def amateur_draft_order(
         cells = row.find_all(["th", "td"])
         row_data = {}
         for header, cell in zip(headers, cells):
-            # Get text content of the cell and strip whitespace
             row_data[header] = cell.get_text(strip=True)
         rows.append(row_data)
     df = pl.DataFrame(rows)
@@ -62,4 +61,89 @@ def amateur_draft_order(
     return df if not return_pandas else df.to_pandas()
 
 
-def franchise_draft_order(team: str, year: int) -> pl.DataFrame | pd.DataFrame: ...
+TEAM_YEAR_DRAFT_URL = "https://www.baseball-reference.com/draft/index.fcgi?team_ID={team}&year_ID={year}&draft_type=junreg&query_type=franch_year&from_type_hs=0&from_type_4y=0&from_type_unk=0&from_type_jc=0"
+
+
+def franchise_draft_order(
+    team: str, year: int, return_pandas: bool = False
+) -> pl.DataFrame | pd.DataFrame:
+    if year < 1965:
+        raise ValueError("Draft data is only available from 1965 onwards")
+    if team not in [
+        "ANA",
+        "ARI",
+        "ATL",
+        "BAL",
+        "BOS",
+        "CHC",
+        "CHW",
+        "CIN",
+        "CLE",
+        "COL",
+        "DET",
+        "FLA",
+        "HOU",
+        "KCR",
+        "LAD",
+        "MIL",
+        "MIN",
+        "NYM",
+        "NYY",
+        "OAK",
+        "PHI",
+        "PIT",
+        "SDP",
+        "SEA",
+        "SFG",
+        "STL",
+        "TBD",
+        "TEX",
+        "TOR",
+        "WSN",
+    ]:
+        raise ValueError("Invalid team abbreviation")
+    resp = requests.get(TEAM_YEAR_DRAFT_URL.format(team=team, year=year))
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+    table = soup.find("table", id="draft_stats")
+
+    headers = [th["data-stat"] for th in table.thead.find_all("th")]
+
+    rows = []
+
+    for row in table.tbody.find_all("tr"):
+        cells = row.find_all(["th", "td"])
+        row_data = {}
+        for header, cell in zip(headers, cells):
+            row_data[header] = cell.get_text(strip=True)
+        rows.append(row_data)
+    df = pl.DataFrame(rows)
+
+    df = df.drop("draft_abb")
+    df = df.with_columns(
+        pl.all().replace("", "0"),
+    )
+    df = df.with_columns(
+        [
+            pl.col("player").str.replace(r"\(minors\)", ""),
+            pl.col(
+                [
+                    "draft_round",
+                    "overall_pick",
+                    "round_pick",
+                    "G_bat",
+                    "AB",
+                    "HR",
+                    "G_pitch",
+                    "year_ID",
+                    "W",
+                    "L",
+                    "SV",
+                ]
+            ).cast(pl.Int32),
+            pl.col(
+                ["WAR", "batting_avg", "onbase_plus_slugging", "earned_run_avg", "whip"]
+            ).cast(pl.Float32),
+        ]
+    )
+    return df if not return_pandas else df.to_pandas()
