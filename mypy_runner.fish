0de5a6ef82befe0f14e93e9b599edb83bc0f9de3
Nico Carbone <carbone.nicolas0@gmail.com>
2026-02-09 18:02:23 +0100

    Remove mypy and test runner scripts; update contribution guidelines for clarity

--- mypy_runner.fish
+++
@@ -1,9 +0,0 @@
-# run mypy type checking
-echo "Running mypy type checking..."
-uv run mypy src/pybaseballstats tests/
-if test $status -eq 0
-    echo "Mypy type checking passed!"
-else
-    echo "Mypy type checking failed!"
-    exit 1
-end
